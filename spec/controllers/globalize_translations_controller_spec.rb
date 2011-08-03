d6aec906b48ff382686c17342a4a9bc075c78db7
Paweł Gościcki <pawel.goscicki@gmail.com>
2011-08-03 11:53:37 +0200

    Refactor controller spec a little

--- spec/controllers/globalize_translations_controller_spec.rb
+++ spec/controllers/globalize_translations_controller_spec.rb
@@ -5,45 +5,53 @@ describe GlobalizeTranslationsController do
 
   let(:i18n_title) { Factory(:i18n_title) }
 
-  describe "#index as JSON" do
+  context "JSON" do
 
-    let(:translations) do
-      i18n_title # needed so there is some data in the database before a call to get :index
-      get :index, :format => :json
-      JSON.parse(response.body)
-    end
+    describe "GET :index" do
 
-    it "returns a non-empty JSON" do
-      translations.present?.should be_true
-    end
+      let(:translations) do
+        i18n_title # needed so there is some data in the database before a call to get :index
+        get :index, :format => :json
+        JSON.parse(response.body)
+      end
 
-    it "returns a JSON with default_locale" do
-      translations["default_locale"].should eq(I18n.default_locale.to_s)
-    end
+      it "returns a non-empty JSON" do
+        translations.present?.should be_true
+      end
 
-    it "returns a JSON with translations" do
-      i18n_title.value.should eq(translations["en"]["title"])
+      it "returns a JSON with default_locale" do
+        translations["default_locale"].should eq(I18n.default_locale.to_s)
+      end
+
+      it "returns a JSON with translations" do
+        i18n_title.value.should eq(translations["en"]["title"])
+      end
+
     end
 
   end
 
-  describe "#index as HTML" do
+  context "HTML" do
 
-    context "without being logged in" do
-      before do
-        get :index, :format => :html
+    describe "GET :index" do
+
+      context "without being logged in" do
+        before do
+          get :index, :format => :html
+        end
+
+        it { should respond_with(:forbidden) }
       end
 
-      it { should respond_with(:forbidden) }
-    end
+      context "when being logged in as admin" do
+        before do
+          controller.stub!(:require_admin).and_return(true)
+          get :index, :format => :html
+        end
 
-    context "when being logged in as admin" do
-      before do
-        controller.stub!(:require_admin).and_return(true)
-        get :index, :format => :html
+        it { should respond_with(:success) }
       end
 
-      it { should respond_with(:success) }
     end
 
   end
