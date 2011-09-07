aa858f5200322042c6060cad457190a94539df15
Paweł Gościcki <pawel.goscicki@gmail.com>
2011-09-07 14:48:09 +0200

    Force translations initialization for the Simple backend
    Since Simple I18n backend seems to be lazy loaded (or it acts this way
    on 1.9.2), there is a need to enforce the loading of YAML files before
    returning the translations Hash.

--- lib/exvo_globalize/backend/simple.rb
+++ lib/exvo_globalize/backend/simple.rb
@@ -6,6 +6,11 @@ module I18n
         # Extend the Simple backend with a custom `available_translations` method
         # returning a combined hash with all translations from this backend
         def available_translations
+          # simple backend is lazy loaded (woken up by first I18n.t() call)
+          # wake it up if it's still sleeping
+          send(:init_translations) unless initialized?
+
+          # nested Hash with all translations
           send(:translations)
         end
       end
