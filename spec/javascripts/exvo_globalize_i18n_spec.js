e370ed5fcbe24c1e5e412a1720578041dcef00ed
Paweł Gościcki <pawel.goscicki@gmail.com>
2011-09-09 15:49:42 +0200

    Mo' better js specs

--- spec/javascripts/exvo_globalize_i18n_spec.js
+++ spec/javascripts/exvo_globalize_i18n_spec.js
@@ -1,7 +1,31 @@
-describe("Translation", function() {
+describe("Basic translation functionality", function() {
 
-  it("should translate the phrase using the t() function", function() {
+  it("translates the phrase using I18n.translate()", function() {
+    expect(I18n.translate("new")).toEqual("New")
+  })
+
+  it("translates the phrase using I18n.t()", function() {
+    expect(I18n.t("new")).toEqual("New")
+  })
+
+  it("translates the phrase using t() global shortcut function", function() {
     expect(t("new")).toEqual("New")
+  })
+
+  it("returns null when translation is not found", function() {
+    expect(I18n.t("non-existing-key")).toBeNull()
+  })
+
+  it("translates the phrase using scoped key with dots as separators", function() {
+    expect(I18n.t("page.intro.heading")).toEqual("Heading")
+  })
+
+  it("translates the phrase using dotted string scope param", function() {
+    expect(I18n.t("heading", {scope: "page.intro"})).toEqual("Heading")
+  })
+
+  it("translates the phrase using Array scope param", function() {
+    expect(I18n.t("heading", {scope: ["page", "intro"]})).toEqual("Heading")
   })
 
 })
--- spec/javascripts/fixtures/translations.js
+++ spec/javascripts/fixtures/translations.js
@@ -1,2 +1,14 @@
 I18n.locale = 'en'
-I18n.translations = { "new": "New" }
+
+I18n.translations = {
+  "new": "New",
+
+  "page": {
+    "footer": "Footer",
+    "title": "Title",
+
+    "intro": {
+      "heading": "Heading"
+    }
+  }
+}
